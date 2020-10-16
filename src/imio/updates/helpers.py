890a4f8eee47914844f8595ef2199e12b52ab591
Stephan Geulette <s.geulette@imio.be>
2020-10-16 08:53:19 +0200

    Moved setup_logger to imio.helpers

--- src/imio/updates/helpers.py
+++ src/imio/updates/helpers.py
@@ -1,21 +1,7 @@
 # -*- coding: utf-8 -*-
-import logging
 from AccessControl.SecurityManagement import newSecurityManager
 from Testing import makerequest
 from zope.globalrequest import setRequest
-
-
-def setup_logger(level=20):
-    """
-        When running "bin/instance run ...", the logger level is 30 (warn).
-        It is possible to set it to 20 (info) or 10 (debug).
-    """
-    logger = logging.getLogger()
-    logger.setLevel(level)
-    for handler in logging.root.handlers:
-        if handler.level == 30 and handler.formatter is not None:
-            handler.level = level
-            break
 
 
 def setup_app(app, username='admin', logger=None):
