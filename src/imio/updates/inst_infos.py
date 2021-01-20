61f34edce0c709b791d22540adbb2d72bee64898
Stephan Geulette <s.geulette@imio.be>
2021-01-20 11:30:52 +0100

    Added wsclient check for dms and pst

--- src/imio/updates/inst_infos.py
+++ src/imio/updates/inst_infos.py
@@ -58,6 +58,18 @@ for index_name, type_names in types_to_count.get(tool, []).items():
     for type_name in type_names:
         infos['types'][type_name] = lengths.get(type_name, 0)
 
+
+def check_wsclient():
+    """Check if wsclient is activated """
+    prefix = 'imio.pm.wsclient.browser.settings.IWS4PMClientSettings'
+    if not api.portal.get_registry_record('{}.pm_url'.format(prefix), default=False):
+        return False
+    gen_act = api.portal.get_registry_record('{}.generated_actions'.format(prefix))
+    if gen_act and gen_act[0].get('permissions') and gen_act[0]['permissions'] != 'Modify view template':
+        return True
+    return False
+
+
 # checks
 if tool == 'pm':
     brains = catalog(meta_type='MeetingConfig')
@@ -71,10 +83,14 @@ if tool == 'dms':
     # get applied workflow adaptations
     from collective.wfadaptations.api import get_applied_adaptations
     applied = ', '.join([d['adaptation'] for d in get_applied_adaptations()])
+    for sub, rep in (('imio.dms.mail.wfadaptations.', ''), ('ServiceValidation', 'sv'), ('ToPrint', 'tp')):
+        applied = applied.replace(sub, rep)
     infos['checks']['wfadaptations'] = applied
     # get assigned_user option
     infos['checks']['assigned_user'] = api.portal.get_registry_record('imio.dms.mail.browser.settings.'
                                                                       'IImioDmsMailConfig.assigned_user_check')
+    # get wsclient option
+    infos['checks']['pm'] = int(check_wsclient())
 
 if tool == 'pst':
     from imio.project.core.content.project import IProject  # noqa
@@ -85,6 +101,8 @@ if tool == 'pst':
         if base_hasattr(obj, 'analytic_budget') and obj.analytic_budget:
             count += 1
     infos['checks']['budget'] = count
+    # get wsclient option
+    infos['checks']['pm'] = int(check_wsclient())
 
 # get users count, only keep users that are in a group
 users = portal.portal_membership.searchForMembers()  # ok with wca
