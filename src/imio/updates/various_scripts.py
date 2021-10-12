a86d6bfd6c9132d2201897a2aa9e7c7c10edaf82
Stephan Geulette <s.geulette@imio.be>
2021-10-12 16:33:44 +0200

    Added import_infos but not working now

--- src/imio/updates/various_scripts.py
+++ src/imio/updates/various_scripts.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import transaction
 
 from imio.helpers.security import setup_logger
 from imio.pyutils.system import error, verbose
 from plone import api
+from Products.CPUtils.Extensions.utils import load_user_properties
+from Products.CPUtils.Extensions.utils import recreate_users_groups
 from Products.CPUtils.Extensions.utils import store_user_properties
 
 # Parameters check
@@ -26,10 +29,22 @@ def export_infos():
 
 def import_infos():
     portal = obj
+    transaction.get()
+    if 'oldacl' not in portal:
+        portal.manage_addFolder('oldacl')
+    oa = portal['oldacl']
+    # if 'users_properties' not in oa:
+    #     oa.manage_importObject('users_properties.zexp')
+    # if 'acl_users' not in oa:
+    #     oa.manage_importObject('acl_users.zexp')
+    # ret = recreate_users_groups(only_users=True, dochange='1')
+    # verbose(ret)
+    # ret = load_user_properties(portal, dochange='1')
+    # verbose(ret)
 
 
 info = ["You can pass following parameters (with the first one always script name):", "export_infos: run ports update"]
-scripts = {'export_infos': export_infos}
+scripts = {'export_infos': export_infos, 'import_infos': import_infos}
 
 if len(sys.argv) < 4 or sys.argv[3] not in scripts:
     error("Bad script parameter")
