bba5ad07e445f0f6a6f2f74c3b9f9d30ed4f2e6e
Stephan Geulette <s.geulette@imio.be>
2024-04-08 16:47:12 +0200

    Used imio.pyutils functions

--- src/imio/updates/run_script.py
+++ src/imio/updates/run_script.py
@@ -1,45 +1,26 @@
 # -*- coding: utf-8 -*-
-
+from imio.pyutils.system import error
+from imio.pyutils.system import load_var
+from imio.pyutils.system import verbose
+from imio.pyutils.utils import setup_logger
 from plone import api
 
-import datetime  # can be in dictionary to eval
 import logging
-import os
 import sys
 import transaction
 
 
 LOGGER_LEVEL = 20  # 30=warning, 20=info, 10=debug
+logger = logging.getLogger()
 
 
-# Copied from imio.pyutils
-def verbose(msg):
-    print '>> %s' % msg
-
-
-def error(msg):
-    print >> sys.stderr, '!! %s' % msg
-
-
-def load_var(infile, var):
-    """
-        load a dictionary or a list from a file
-    """
-    if os.path.exists(infile):
-        ofile = open(infile, 'r')
-        if isinstance(var, dict):
-            var.update(eval(ofile.read()))
-        elif isinstance(var, list):
-            var.extend(eval(ofile.read()))
-        ofile.close()
-
-
-def setup_logger(level=20):
+# could be imported from imio.pyutils or imio.helpers: TODO test it
+def setup_logger_orig(level=20):
     """
         When running "bin/instance run ...", the logger level is 30 (warn).
         It is possible to set it to 20 (info) or 10 (debug).
     """
-    logger = logging.getLogger()
+    logger = logging.getLogger()  # noqa
     logger.setLevel(level)
     for handler in logging.root.handlers:
         if handler.level == 30 and handler.formatter is not None:
@@ -47,7 +28,7 @@ def setup_logger(level=20):
             break
 
 if LOGGER_LEVEL != 30:
-    setup_logger(LOGGER_LEVEL)
+    setup_logger(logger, level=LOGGER_LEVEL)
 
 # Parameters check
 if len(sys.argv) < 3 or not sys.argv[2].endswith('run_script.py'):
@@ -157,6 +138,7 @@ def message():
     except Exception:
         pass
     transaction.commit()
+
 
 info = ["You can pass following parameters (with the first one always script number):", "step: run profile step",
         "upgrade: run profile upgrade", "auth: enable/disable authentication plugins", "message"]
