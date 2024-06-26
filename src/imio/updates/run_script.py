a8c00e433681cf4ad700a193570dda2199eb5883
Stephan Geulette <s.geulette@imio.be>
2024-06-26 12:23:43 +0200

    Cleaned run_script

--- src/imio/updates/run_script.py
+++ src/imio/updates/run_script.py
@@ -5,6 +5,7 @@ from imio.pyutils.system import verbose
 from imio.pyutils.utils import setup_logger
 from plone import api
 
+import datetime  # can be in dictionary to eval  # noqa
 import logging
 import sys
 import transaction
@@ -14,24 +15,11 @@ LOGGER_LEVEL = 20  # 30=warning, 20=info, 10=debug
 logger = logging.getLogger()
 
 
-# could be imported from imio.pyutils or imio.helpers: TODO test it
-def setup_logger_orig(level=20):
-    """
-        When running "bin/instance run ...", the logger level is 30 (warn).
-        It is possible to set it to 20 (info) or 10 (debug).
-    """
-    logger = logging.getLogger()  # noqa
-    logger.setLevel(level)
-    for handler in logging.root.handlers:
-        if handler.level == 30 and handler.formatter is not None:
-            handler.level = level
-            break
-
 if LOGGER_LEVEL != 30:
     setup_logger(logger, level=LOGGER_LEVEL)
 
 # Parameters check
-if len(sys.argv) < 3 or not sys.argv[2].endswith('run_script.py'):
+if len(sys.argv) < 3 or not sys.argv[2].endswith("run_script.py"):
     error("Inconsistent or unexpected args len: %s" % sys.argv)
     sys.exit(0)
 
@@ -42,19 +30,20 @@ def run_step():
         sys.exit(0)
     profile = sys.argv[4]
     step = sys.argv[5]
-    if not profile.startswith('profile-'):
-        profile = 'profile-%s' % profile
+    if not profile.startswith("profile-"):
+        profile = "profile-%s" % profile
     # obj is plone site
-    if step == '_all_':
+    obj = obj  # noqa
+    if step == "_all_":
         verbose('Running all "%s" steps on %s' % (profile, obj.absolute_url_path()))
         ret = obj.portal_setup.runAllImportStepsFromProfile(profile)
     else:
         verbose('Running "%s#%s" step on %s' % (profile, step, obj.absolute_url_path()))
         ret = obj.portal_setup.runImportStepFromProfile(profile, step, run_dependencies=False)
 
-    if 'messages' in ret:
-        for step in ret['messages']:
-            verbose("%s:\n%s" % (step, ret['messages'][step]))
+    if "messages" in ret:
+        for step in ret["messages"]:
+            verbose("%s:\n%s" % (step, ret["messages"][step]))
     else:
         verbose("No output")
     transaction.commit()
@@ -68,9 +57,10 @@ def run_upgrade():
     from imio.migrator.migrator import Migrator
 
     # obj is plone site
+    obj = obj  # noqa
     mig = Migrator(obj)
-    if profile == '_all_':
-        verbose('Running all upgrades on %s' % (obj.absolute_url_path()))
+    if profile == "_all_":
+        verbose("Running all upgrades on %s" % (obj.absolute_url_path()))
         mig.upgradeAll()
     else:
         verbose('Running "%s" upgrade on %s' % (profile, obj.absolute_url_path()))
@@ -82,28 +72,30 @@ def auth():
     from Products.ExternalMethod.ExternalMethod import manage_addExternalMethod
 
     # we add the external method cputils_install
-    if not hasattr(app, 'cputils_install'):
-        manage_addExternalMethod(app, 'cputils_install', '', 'CPUtils.utils', 'install')
+    app = app  # noqa
+    if not hasattr(app, "cputils_install"):
+        manage_addExternalMethod(app, "cputils_install", "", "CPUtils.utils", "install")
     # we run this method
     app.cputils_install(app)
     # change authentication
     status = sys.argv[4]
-    app.cputils_change_authentication_plugins(activate=status, dochange='1')
-    verbose("Authentication plugins %s" % (status == '0' and 'disabled' or 'enabled'))
+    app.cputils_change_authentication_plugins(activate=status, dochange="1")
+    verbose("Authentication plugins %s" % (status == "0" and "disabled" or "enabled"))
     transaction.commit()
 
 
 def message():
     from collective.messagesviewlet.utils import _richtextval
     from collective.messagesviewlet.utils import add_message
+
     id, warning_file = sys.argv[4:6]
     dic = {}
     load_var(warning_file, dic)
     params = dic[id]
-    delete = params.pop('delete', False)
+    delete = params.pop("delete", False)
     modified = False
-    #verbose("Given dictionary: %s" % params)
-    config = obj['messages-config']
+    # verbose("Given dictionary: %s" % params)
+    config = obj["messages-config"]  # noqa
     if delete:
         if id in config:
             api.content.delete(config[id])
@@ -112,42 +104,47 @@ def message():
     elif id in config:
         msg = config[id]
         verbose("Updating message with those values: %s" % params)
-        if 'text' in params:
-            msg.text = _richtextval(params.pop('text'))
+        if "text" in params:
+            msg.text = _richtextval(params.pop("text"))
     # create a new message
     else:
         verbose("Creating message with those values: %s" % params)
         attrs = {}
-        attrs.get('msg_type', 'warning')
-        msg = add_message(id, params.get('title', 'imio.updates'), params['text'], **attrs)
+        attrs.get("msg_type", "warning")
+        msg = add_message(id, params.get("title", "imio.updates"), params["text"], **attrs)
 
     for key in params:
-        if key not in ('text', 'activate'):
+        if key not in ("text", "activate"):
             setattr(msg, key, params[key])
             modified = True
     if modified:
         msg.reindexObject()
 
     try:
-        if params.get('activate', True):
-            api.content.transition(msg, transition='activate')
+        if params.get("activate", True):
+            api.content.transition(msg, transition="activate")
             verbose("Message %s activated" % msg)
         else:
-            api.content.transition(msg, transition='deactivate')
+            api.content.transition(msg, transition="deactivate")
             verbose("Message %s deactivated" % msg)
     except Exception:
         pass
     transaction.commit()
 
 
-info = ["You can pass following parameters (with the first one always script number):", "step: run profile step",
-        "upgrade: run profile upgrade", "auth: enable/disable authentication plugins", "message"]
-scripts = {'step': run_step, 'upgrade': run_upgrade, 'auth': auth, 'message': message}
+info = [
+    "You can pass following parameters (with the first one always script number):",
+    "step: run profile step",
+    "upgrade: run profile upgrade",
+    "auth: enable/disable authentication plugins",
+    "message",
+]
+scripts = {"step": run_step, "upgrade": run_upgrade, "auth": auth, "message": message}
 
 if len(sys.argv) < 4 or sys.argv[3] not in scripts:
     error("Bad script parameter")
-    verbose('\n>> =>'.join(info))
+    verbose("\n>> =>".join(info))
     sys.exit(0)
 
-with api.env.adopt_user(username='admin'):
+with api.env.adopt_user(username="admin"):
     scripts[sys.argv[3]]()
